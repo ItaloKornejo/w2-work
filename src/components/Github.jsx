fe92e5bfaad2427d661997be8998c026cee0d0dd
ItaloKornejo <103982201+ItaloKornejo@users.noreply.github.com>
2022-09-25 07:51:06 -0500

    feat: link repository

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Github = () => {
   return (
     <div className='repository'>
-        <a href='https://github.com/ItaloKornejo/w2-work' className='link-repository'><i className='bx bxl-github'></i></a>
+        <a href='https://github.com/ItaloKornejo/w2-work' target='_blank' className='link-repository'><i className='bx bxl-github'></i></a>
     </div>
   )
 }
